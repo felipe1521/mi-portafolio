13060fad388b55e96436f304a88862549a32ea05
felipe1521 <felipealarcon44@gmail.com>
2024-07-11 22:27:47 -0400

    se agregan features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    { path: '**', component: HomeComponent }
+    {path: 'experience', component: ExperienceComponent}
 ];
---
+++ src/app/components/experience/experience.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './experience.component.html',
+  styleUrl: './experience.component.scss'
+})
+export class ExperienceComponent {
+
+}
