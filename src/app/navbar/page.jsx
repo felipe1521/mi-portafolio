a0fbdb58c972b0160aa9f2b68411ad1d05e60d5d
felipe1521 <felipealarcon44@gmail.com>
2024-08-21 16:48:24 -0400

    se corrige navbar

--- src/app/navbar/page.jsx
+++ src/app/navbar/page.jsx
@@ -4,13 +4,17 @@ import './module.css';
 export default function Navbar() {
   return (
     <header className="header">
-        <h1 className="logo">Portafolio</h1>
-        <i className="bx bx-menu" id="menu-icon"></i>
+        <a href="#" className="logo">Portafolio</a>
+        <input type="checkbox" id="check" />
+        <label htmlFor="check" className="icons">
+          <i className="bx bx-menu" id="menu-icon"></i>
+          <i className="bx bx-x" id="close-icon"></i>
+        </label>
         <nav className="navbar">
-            <a href="#">Inicio</a>
-            <a href="#">Experiencia</a>
-            <a href="#">Educacion</a>
-            <a href="#">Habilidades</a>
+            <a href="#section1" className="a0">Inicio</a>
+            <a href="#section2" className="a1">Experiencia</a>
+            <a href="#section3" className="a2">Educacion</a>
+            <a href="#section4" className="a3">Habilidades</a>
         </nav>
     </header>
   );
