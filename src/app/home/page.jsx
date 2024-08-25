704962674fc57641ebf705135dc5dafe5e414849
felipe1521 <felipealarcon44@gmail.com>
2024-08-25 16:34:54 -0400

    se corrige diseno responsivo

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -23,20 +23,20 @@ export default function Home() {
           <a href="https://linkedin.com/in/felipe-ignacio-alarcon-contreras-670a25286/" target="_blank"
           style={{ backgroundColor: "#0A66C2", boxShadow: "0 0 1rem #0A66C2"}}>
             <i className="bx bxl-linkedin"></i>
-            <b>Linkedin</b>
+            <p>Linkedin</p>
           </a>
           <a href="https://github.com/felipe1521" target="_blank"
           style={{ backgroundColor: "#0D1117", boxShadow: "0 0 1rem #0D1117"}}>
             <i className="bx bxl-github"></i>
-            <b>Github</b>
+            <p>Github</p>
           </a>
           <a style={{ backgroundColor: "#C82401", boxShadow: "0 0 1rem #C82401"}}>
             <i className="bx bxl-gmail"></i>
-            <b>felipealarcon44@gmail.com</b>
+            <p>felipealarcon44@gmail.com</p>
           </a>
           <a style={{ backgroundColor: "#25D366", boxShadow: "0 0 1rem #25D366"}}>
             <i className="bx bxl-whatsapp"></i>
-            <b>+56950655634</b>
+            <p>+56950655634</p>
           </a>
         </div>
         <a href="/CV-FelipeAlarconContreras.pdf" target="_blank" className="btn">Revisa mi CV</a>
