c16d78c011b254e78ccd228900cafdcf87e03a99
felipe1521 <felipealarcon44@gmail.com>
2024-11-07 13:16:39 -0300

    se reorganiza el proyecto

--- src/app/components/links/Links.jsx
+++ src/app/components/links/Links.jsx
@@ -15,14 +15,14 @@ function Links() {
           <i className="bx bxl-github"></i>
           <p>Github</p>
         </a>
-        <a className={styles.link} style={{ backgroundColor: "#C82401", boxShadow: "0 0 1rem #C82401"}}>
-          <i className="bx bxl-gmail"></i>
-          <p>felipealarcon44@gmail.com</p>
-        </a>
-        <a className={styles.link} style={{ backgroundColor: "#25D366", boxShadow: "0 0 1rem #25D366"}}>
-          <i className="bx bxl-whatsapp"></i>
-          <p>+56950655634</p>
-        </a>
+      </div>
+      <div className={styles.data_content} style={{ backgroundColor: "#C82401", boxShadow: "0 0 1rem #C82401"}}>
+        <i className="bx bxl-gmail"></i>
+        <p>felipealarcon44@gmail.com</p>
+      </div>
+      <div className={styles.data_content} style={{ backgroundColor: "#25D366", boxShadow: "0 0 1rem #25D366"}}>
+        <i className="bx bxl-whatsapp"></i>
+        <p>+56950655634</p>
       </div>
       <button href="/CV-FelipeAlarconContreras.pdf" target="_blank" className={styles.btn}>Revisa mi CV</button>
     </div>
